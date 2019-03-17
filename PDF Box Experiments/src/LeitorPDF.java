ae05e22bcb10edfd13947b6bddf47d2285640b99
pedropk <pedropk@gmail.com>
2019-03-17 01:53:49 -0300

    Fulfilling with Test Cases

--- PDF Box Experiments/src/LeitorPDF.java
+++ PDF Box Experiments/src/LeitorPDF.java
@@ -4,16 +4,25 @@ import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import org.apache.pdfbox.text.PDFTextStripper;
-import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
 
 public class LeitorPDF {
+	
+	public static final String PDF_EXTENSION = ".pdf";
 	
 	public static PDDocument getPdDocument(String pFilePath) throws InvalidPasswordException, IOException {
 		File arquivo = new File(pFilePath);
 		
 		PDDocument documento = PDDocument.load(arquivo);
+		return documento;
+	}
+	
+	public static PDDocument getPdDocumentEncrypted(
+		String		pFilePath,
+		String		pPassword)
+	throws InvalidPasswordException, IOException {
+		File arquivo = new File(pFilePath);
+		
+		PDDocument documento = PDDocument.load(arquivo, pPassword);
 		return documento;
 	}
 	
@@ -24,4 +33,54 @@ public class LeitorPDF {
 		String texto = pdfStripper.getText(documento);
 		return texto;
 	}
+	
+	public static String[] getPrefixArray(String pOriginalFileName) {
+		String[] fileName = pOriginalFileName.split(PDF_EXTENSION);
+		return fileName;
+	}
+	
+	public static String getDecryptedFileName(String[] pFileName) {
+		return pFileName[0] + "_decrypted" + LeitorPDF.PDF_EXTENSION;
+	}
+	
+	public static PDDocument removeEncryption(String pPath, String pPassword) throws InvalidPasswordException, IOException {
+		PDDocument documento = 
+			LeitorPDF.getPdDocumentEncrypted(
+				pPath,
+				pPassword 
+		);
+		
+		documento.setAllSecurityToBeRemoved(true);
+		return documento;
+	}
+	
+	public static File getDecryptedFile(String pPath, String pPassword) throws InvalidPasswordException, IOException {
+		PDDocument documento = 
+			LeitorPDF.removeEncryption(
+				pPath,
+				pPassword 
+		);
+		
+		File decryptedFile = new File(pPath + "_decrypted");
+		
+		return decryptedFile;
+	}
+	
+	public void saveDecryptedFile(
+		String	pFilePath,
+		String	pPassword)
+	throws InvalidPasswordException, IOException {
+		PDDocument documento = 
+			LeitorPDF.getPdDocumentEncrypted(
+				pFilePath,
+				pPassword 
+		);
+		
+		documento.setAllSecurityToBeRemoved(true);
+		
+		File decryptedFile = new File(pFilePath + "_decrypted");
+		
+		documento.save(decryptedFile);
+	}
+	
 }
--- PDF Box Experiments/tsw/TestLeitorPDF.java
+++ PDF Box Experiments/tsw/TestLeitorPDF.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
-import org.apache.pdfbox.text.PDFTextStripper;
-import org.junit.Assert;
+import static org.junit.Assert.*;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -12,30 +12,39 @@ public class TestLeitorPDF {
 	
 	private static final String PATH_PDF_PROPOSTA_HABILITACAO_PITANG = "/Users/pedropk/Downloads/Temp/eFisco/pitangpropostahabilitacao02.pdf";
 	private static final String PATH_PDF_POLICENTRO_CONTRA_CHEQUE_2019_02_FEVEREIRO = "/Users/pedropk/Downloads/Temp/eFisco/PEDRO CARLOS SANTOS - 2019 02 Fevereiro - Policentro.pdf";
+	private static final String PATH_PDF_EXTRATO_B3_2019_03_MARCO = "/Users/pedropk/Downloads/Temp/B3/0000278765_00308_0201466.pdf";
+	
+	/**
+	 * REPLACE_WITH_REAL_PASSWORD
+	 * 
+	 * @throws InvalidPasswordException
+	 * @throws IOException
+	 */
+	private static final String PASSWORD = "";
 	
 	@Test
 	public void testTentarCriarArquivoPropostaHabilitacaoPitang() {
 		File arquivo = new File(PATH_PDF_PROPOSTA_HABILITACAO_PITANG);
 		
 		
-		Assert.assertNotNull(arquivo);
+		assertNotNull(arquivo);
 		
 	}
 	
 	@Test
 	public void testTentarCriarPDDocumentPropostaHabilitacaoPitang() throws InvalidPasswordException, IOException {
 		PDDocument documento = LeitorPDF.getPdDocument(PATH_PDF_PROPOSTA_HABILITACAO_PITANG);
-		Assert.assertNotNull(documento);
+		assertNotNull(documento);
 	}
 	
 	@Test
 	public void testTentarLerPropostaHabilitacaoPitang() throws InvalidPasswordException, IOException {
 		String texto = LeitorPDF.getTexto(PATH_PDF_PROPOSTA_HABILITACAO_PITANG);
 		
-		Assert.assertNotNull(texto);
-		Assert.assertFalse(texto.isEmpty());
+		assertNotNull(texto);
+		assertFalse(texto.isEmpty());
 		
-		System.out.println(texto.length());
+		//System.out.println(texto.length());
 		//System.out.println(texto);
 	}
 	
@@ -44,25 +53,93 @@ public class TestLeitorPDF {
 		File arquivo = new File(PATH_PDF_POLICENTRO_CONTRA_CHEQUE_2019_02_FEVEREIRO);
 		
 		
-		Assert.assertNotNull(arquivo);
+		assertNotNull(arquivo);
 		
 	}
 	
 	@Test
 	public void testTentarCriarPDDocumentPolicentroContraCheque_2019_02Fevereiro() throws InvalidPasswordException, IOException {
 		PDDocument documento = LeitorPDF.getPdDocument(PATH_PDF_POLICENTRO_CONTRA_CHEQUE_2019_02_FEVEREIRO);
-		Assert.assertNotNull(documento);
+		assertNotNull(documento);
 	}
 	
 	@Test
 	public void testTentarLerPolicentroContraCheque_2019_02Fevereiro() throws InvalidPasswordException, IOException {
 		String texto = LeitorPDF.getTexto(PATH_PDF_POLICENTRO_CONTRA_CHEQUE_2019_02_FEVEREIRO);
 		
-		Assert.assertNotNull(texto);
-		Assert.assertFalse(texto.isEmpty());
+		assertNotNull(texto);
+		assertFalse(texto.isEmpty());
 		
-		System.out.println(texto.length());
-		System.out.println(texto);
+		/*System.out.println(texto.length());
+		System.out.println(texto);*/
+	}
+	
+	@Test
+	public void testPdDocumentoEncryptedNotNullAndSecurityEnabled() throws InvalidPasswordException, IOException {
+		PDDocument documento = 
+			LeitorPDF.getPdDocumentEncrypted(
+				PATH_PDF_EXTRATO_B3_2019_03_MARCO,
+				PASSWORD 
+		);
+		
+		assertNotNull(documento);
+		assertFalse(documento.isAllSecurityToBeRemoved());
+	}
+	
+	@Test
+	public void testPdDocumentoEncryptedSucessfullySecurityRemoved() throws InvalidPasswordException, IOException {
+		String path = PATH_PDF_EXTRATO_B3_2019_03_MARCO;
+		String password = PASSWORD;
+		
+		PDDocument documento = LeitorPDF.removeEncryption(path, password);
+		
+		assertTrue(documento.isAllSecurityToBeRemoved());
+	}
+	
+	@Test
+	public void testDecryptedFileNotNull() throws InvalidPasswordException, IOException {
+		String path = PATH_PDF_EXTRATO_B3_2019_03_MARCO;
+		String password = PASSWORD;
+		
+		File decryptedFile = LeitorPDF.getDecryptedFile(path, password);
+		
+		assertNotNull(decryptedFile);
+	}
+	
+	@Test
+	public void testDecryptedFileSavedSucessfully() throws InvalidPasswordException, IOException {
+		PDDocument documento = 
+			LeitorPDF.getPdDocumentEncrypted(
+				PATH_PDF_EXTRATO_B3_2019_03_MARCO,
+				PASSWORD 
+		);
+		
+		documento.setAllSecurityToBeRemoved(true);
+		
+		File decryptedFile = new File(PATH_PDF_EXTRATO_B3_2019_03_MARCO + "_decrypted");
+		documento.save(decryptedFile);
+	}
+	
+	@Test
+	public void getPrefixArrayFromFileName() {
+		String originalFileName = PATH_PDF_EXTRATO_B3_2019_03_MARCO;
+		String[] fileName = LeitorPDF.getPrefixArray(originalFileName);
+		
+		
+		
+		assertNotNull(fileName);
+		assertEquals(1, fileName.length);
+	}
+	
+	@Ignore
+	@Test
+	public void testDecryptedFileNameContainsOriginalFileName() {
+		String originalFileName = PATH_PDF_EXTRATO_B3_2019_03_MARCO;
+		String decryptedFileName = 
+			LeitorPDF.getDecryptedFileName(
+				LeitorPDF.getPrefixArray(originalFileName));
+		
+		//assertTrue( decryptedFileName.contains(originalFileName.) );
 	}
 	
 }
