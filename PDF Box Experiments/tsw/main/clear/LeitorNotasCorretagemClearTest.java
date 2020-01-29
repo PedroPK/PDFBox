92fb80214962ec64d4e6473aef7fd91020d55f3c
pedro.carlos.santos <pedro.carlos.santos@CPX-6BIIS9Q7KYX.dir.svc.accenture.com>
2020-01-29 13:55:35 -0300

    Stub Test Methods for GetAllFilesInDirectory() created.

--- PDF Box Experiments/tsw/main/clear/LeitorNotasCorretagemClearTest.java
+++ PDF Box Experiments/tsw/main/clear/LeitorNotasCorretagemClearTest.java
@@ -1,6 +1,12 @@
 package main.clear;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+import java.util.List;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -46,19 +52,104 @@ class LeitorNotasCorretagemClearTest {
 	@Disabled
 	@Test
 	void testGetFile() {
+		// Arrange
+		
+		
+		// Act
+		
+		
+		// Assert
+		
 		fail("Not yet implemented");
 	}
 	
 	@Disabled
 	@Test
 	void testGetPdfDocument() {
+		// Arrange
+		
+		
+		// Act
+		
+		
+		// Assert
 		fail("Not yet implemented");
 	}
 	
 	@Disabled
 	@Test
 	void testReadLine() {
+		// Arrange
+		
+		
+		// Act
+		
+		
+		// Assert
+		
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetAllFilesInDirectory_NotNull() {
+		// Arrange
+		ILeitorNotasCorretagemClear leitorNotasClear = new LeitorNotasCorretagemClear();
+		
+		// Act
+		List<File> result = leitorNotasClear.getAllFilesInDirectory();
+		
+		// Assert
+		assertNotNull(result);
+	}
+	
+	@Test
+	void testGetAllFilesInDirectory_NotEmpty() {
+		// Arrange
+		ILeitorNotasCorretagemClear leitorNotasClear = new LeitorNotasCorretagemClear();
+		
+		// Act
+		List<File> result = leitorNotasClear.getAllFilesInDirectory();
+		
+		// Assert
+		assertFalse(result.isEmpty());
+	}
+	
+	@Test
+	void testGetAllFilesInDirectory_OnlyFilesOnList() {
+		// Arrange
+		ILeitorNotasCorretagemClear leitorNotasClear = new LeitorNotasCorretagemClear();
+		
+		// Act
+		List<File> result = leitorNotasClear.getAllFilesInDirectory();
+		
+		// Assert
+		for (File file : result) {
+			if ( !file.isFile() ) {
+				fail("There is a Non File in the List");
+			}
+		}
+	}
+	
+	@Test
+	void testGetAllFilesInDirectory_AllFilesArePDFs() {
+		// Arrange
+		ILeitorNotasCorretagemClear leitorNotasClear = new LeitorNotasCorretagemClear();
+		
+		// Act
+		List<File> result = leitorNotasClear.getAllFilesInDirectory();
+		
+		// Assert
+		for (File file : result) {
+			String fileExtension = 
+				file.getName().substring(
+					file.getName().length() - 3, 
+					file.getName().length()
+			);
+			
+			if ( !fileExtension.equalsIgnoreCase("PDF") ) {
+				fail("There is a Non PDF File in the List");
+			}
+		}
 	}
 
 }
